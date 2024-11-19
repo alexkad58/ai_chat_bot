e4c774ae4b9153b73e5cc7f679b208d9c73a7ada
alexkad58 <37246064+alexkad58@users.noreply.github.com>
2024-11-19 23:26:55 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const config = require('./config.json');
+const config = require('./config.json')
 require('dotenv').config()
 const { Api, TelegramClient } = require("telegram");
 const { StringSession } = require("telegram/sessions");
