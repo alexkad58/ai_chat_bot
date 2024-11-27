7b98fa711fb3c30f26d46439bee878839e9ab916
alexkad58 <37246064+alexkad58@users.noreply.github.com>
2024-11-27 19:12:55 +0300

    Update eval.js

--- src/handlers/commands/eval.js
+++ src/handlers/commands/eval.js
@@ -1,4 +1,5 @@
 const sendMessage = require("../../utils/sendMessage");
+const logger = require("../utils/logger");
 
 module.exports = {
     name: 'eval',
@@ -7,7 +8,7 @@ module.exports = {
         if (!args[0]) return sendMessage('[bot] Напишите код для выполнения', chatInput, client, event.message.id);
         const code = args.join(' ')
         try {
-            eval(code)
+            await eval(code)
             await sendMessage(`[bot] Код выполнен.`, chatInput, client, event.message.id);
         } catch (error) {
             await sendMessage(`[bot] Ошибка при выполнении кода: ${error.message}`, chatInput, client, event.message.id);
